275f629351e7dfff069607a51d37141b4ebc3e51
kaorin4 <kaori.murakami4@gmail.com>
2020-10-01 12:21:58 -0700

    added project structure

---
+++ instaISIL/instaISIL/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaISIL
+//
+//  Created by Kaori on 10/1/20.
+//  Copyright © 2020 Kaori Murakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ instaISIL/instaISILTests/instaISILTests.swift
@@ -0,0 +1,34 @@
+//
+//  InstaISILTests.swift
+//  InstaISILTests
+//
+//  Created by Kaori on 10/1/20.
+//  Copyright © 2020 Kaori Murakami. All rights reserved.
+//
+
+import XCTest
+@testable import InstaISIL
+
+class InstaISILTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
