8d05d8c0c8a153639c8b24f0fd7302bffe6f9c4e
Kaori Murakami <kaori.murakami4@gmail.com>
2020-10-01 14:43:55 -0700

    added basic login layout

---
+++ instaISIL/instaISIL/Controller/LoginViewController.swift
@@ -0,0 +1,92 @@
+//
+//  LoginViewController.swift
+//  InstaISIL
+//
+//  Created by user178634 on 10/1/20.
+//  Copyright © 2020 Kaori Murakami. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var constraintCenterYContent: NSLayoutConstraint!
+    
+    @IBOutlet weak var viewContent: UIView!
+    
+    @IBAction func tapDismissKeyboard(_ gestureRecognizer : UITapGestureRecognizer) {
+        self.view.endEditing(true)        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        let endPosYContent = self.viewContent.frame.origin.y + self.viewContent.frame.height
+        let originKeyboardY = keyboardFrame.origin.y
+        var spaceKeyboardToViewContent: CGFloat = 20
+        var delta: CGFloat = 0
+        
+        if originKeyboardY < endPosYContent {
+            delta = originKeyboardY - endPosYContent - spaceKeyboardToViewContent
+        }
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.constraintCenterYContent.constant = delta
+            self.view.layoutIfNeeded()
+        }
+        
+        
+        
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        UIView.animate(withDuration: animationDuration) {
+            
+            self.constraintCenterYContent.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
