b47a4bb8f7ae088010cafc34e898bf1cbef94818
Kaori Murakami <kaori.murakami4@gmail.com>
2020-10-03 16:31:58 -0700

    added basic signup screen layout

---
+++ instaISIL/instaISIL/Controller/SignUpViewController.swift
@@ -0,0 +1,94 @@
+//
+//  SignUpViewController.swift
+//  InstaISIL
+//
+//  Created by user178634 on 10/3/20.
+//  Copyright © 2020 Kaori Murakami. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var constraintBottonScrollView: NSLayoutConstraint!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBAction func SignUpButtonPressed(_ sender: Any) {
+        
+        let email: String = emailTextField.text ?? ""
+        let isEmail: Bool = CommonUtility.isValidEmail(email)
+        if !isEmail {
+            statusLabel.isHidden = false
+            statusLabel.text = "Ingresar email válido"
+        } else {
+            statusLabel.text = ""
+            statusLabel.isHidden = true
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.constraintBottonScrollView.constant = keyboardFrame.size.height
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        UIView.animate(withDuration: animationDuration) {
+            
+            self.constraintBottonScrollView.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- instaISIL/instaISIL/Utility/Common.swift
+++ instaISIL/instaISIL/Utility/Common.swift
@@ -12,7 +12,7 @@ import Foundation
 class CommonUtility{
     
     static func isValidEmail(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailRegEx: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
             
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: email)
